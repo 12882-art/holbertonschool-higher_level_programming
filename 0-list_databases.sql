f136155081439aa08ebdc40d5ffcb67384b5e6ab
12882-art <12882@holbertonstudents.com>
2026-01-23 13:45:39 -0600

    Update 0-list_databases.sql

--- 0-list_databases.sql
+++ 0-list_databases.sql
@@ -1,8 +1,2 @@
-guillaume@ubuntu:~/$ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
-Enter password: 
-Database                                                                                     
-information_schema                                                                           
-mysql                                                                                        
-performance_schema                                                                           
-sys        
-guillaume@ubuntu:~/$ 
+-- Lists all databases of MySQL server
+SHOW DATABASES;
