cedd30095b7543238f053bba46dc1eb11ef637f8
deita_m <12882@holbertonstudents.com>
2026-01-27 15:30:01 -0600

    Create 2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates database hbtn_od_2 and user user_od_2 with SELECT privilege
+CREATE DATABASE IF NOT EXISTS hbtn_od_2;
+CREATE USER IF NOT EXISTS 'user_od_2'@'localhost' IDENTIFIED BY 'user_od_2_pwd';
+GRANT SELECT ON hbtn_od_2.* TO 'user_od_2'@'localhost';
