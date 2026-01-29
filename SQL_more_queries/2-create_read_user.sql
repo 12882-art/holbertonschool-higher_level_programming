a93d624d25fa5a340b5d13c052cf06d2a49d28f2
deita_m <12882@holbertonstudents.com>
2026-01-29 15:35:48 -0600

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates database hbtn_od_2 and user user_od_2
-CREATE DATABASE IF NOT EXISTS hbtn_od_2;
-CREATE USER IF NOT EXISTS 'user_od_2'@'localhost' IDENTIFIED BY 'user_od_2_pwd';
-GRANT SELECT ON hbtn_od_2.* TO 'user_od_2'@'localhost';
+-- Creates database hbtn_ed_2 and user user_ed_2
+CREATE DATABASE IF NOT EXISTS hbtn_ed_2;
+CREATE USER IF NOT EXISTS 'user_ed_2'@'localhost' IDENTIFIED BY 'user_ed_2_pwd';
+GRANT SELECT ON hbtn_ed_2.* TO 'user_ed_2'@'localhost';
