ee570cead292eab6c8b29da56b889948cce25244
deita_m <12882@holbertonstudents.com>
2026-01-29 15:38:22 -0600

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Creates database hbtn_ed_2 and user user_ed_2
+-- EXACTO como en la imagen que me pasaste
 CREATE DATABASE IF NOT EXISTS hbtn_ed_2;
 CREATE USER IF NOT EXISTS 'user_ed_2'@'localhost' IDENTIFIED BY 'user_ed_2_pwd';
 GRANT SELECT ON hbtn_ed_2.* TO 'user_ed_2'@'localhost';
