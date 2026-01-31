2145db18280cd18b0f318e26405e10240582419d
deita_m <12882@holbertonstudents.com>
2026-01-30 21:02:09 -0600

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,4 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS hbtn_ed_2;
-
--- Create user if not exists
-CREATE USER IF NOT EXISTS 'user_ed_2'@'localhost' IDENTIFIED BY 'user_ed_2_pwd';
-
--- Grant SELECT privilege on hbtn_ed_2 to user_ed_2
-GRANT SELECT ON hbtn_ed_2.* TO 'user_ed_2'@'localhost';
+-- script creates database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
