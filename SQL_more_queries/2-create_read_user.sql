633a0f1117a11e3610491b0d7f12019fe9a7ffbe
deita_m <12882@holbertonstudents.com>
2026-01-30 20:46:13 -0600

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,8 @@
--- Creates database hbtn_ed_2 and user user_ed_2
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_ed_2;
+
+-- Create user if not exists
 CREATE USER IF NOT EXISTS 'user_ed_2'@'localhost' IDENTIFIED BY 'user_ed_2_pwd';
+
+-- Grant SELECT privilege on hbtn_ed_2 to user_ed_2
 GRANT SELECT ON hbtn_ed_2.* TO 'user_ed_2'@'localhost';
